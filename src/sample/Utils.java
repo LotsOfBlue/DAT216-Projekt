12df9e126494bb8d7254066df2ee29e0eba0a6cc
Daniel Hesslow <Daniel.hesslow@gmail.com>
2016-02-24 09:34:15 +0100

    asd

--- src/sample/Utils.java
+++ src/sample/Utils.java
@@ -7,6 +7,12 @@ public class Utils
 {
     public static int levDist(String a, String b)
     {
-
+        int ack = 0;
+        for(int i = 0; i<a.length();i++)
+        {
+            if(a.charAt(i)!=b.charAt(i))
+                ack++;
+        }
+        return ack;
     }
 }
