7e3fc758038d398be31c38dc80ee8c8407d5563a
Daniel Hesslow <Daniel.hesslow@gmail.com>
2016-02-25 17:00:38 +0100

    Controller is linked

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,32 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+    @FXML
+    Button meatButton;
+    @FXML
+    Button fishButton;
+    @FXML
+    Button drinkButton;
+    @FXML
+    Button sweetsButton;
+    @FXML
+    Button fruitButton;
+    @FXML
+    Button breadButton;
+    @FXML
+    Button spiceButton;
+
+
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,16 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
