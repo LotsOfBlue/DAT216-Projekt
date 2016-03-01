ba0c85027f6ff67e0fc2002710633bcb6de50741
Johan Blomberg <jccblomberg@gmail.com>
2016-03-01 17:51:34 +0100

    Added a few more textfields to Checkout

--- src/sample/CheckOutController.java
+++ src/sample/CheckOutController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
@@ -22,7 +23,13 @@ public class CheckOutController implements Initializable
     public Label amount_label;
     @FXML
     public Label sum_label;
-
+    public TextField firstNameField;
+    public TextField lastNameField;
+    public TextField addressField;
+    public TextField locationLabel;
+    public TextField postcodeLabel;
+    public TextField phoneLabel;
+    
     @Override
     public void initialize(URL fxmlFileLocation, ResourceBundle resources)
     {
