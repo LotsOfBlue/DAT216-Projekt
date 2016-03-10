1f6270b9ad121a0c33707cdd92e4d0eb567a3202
Daniel Hesslow <Daniel.hesslow@gmail.com>
2016-03-10 13:16:01 +0100

    again

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -101,6 +101,8 @@ public class Main extends Application {
         Pane purchaseCompletePane = loader.load(getClass().getResource("PurchaseComplete.fxml").openStream());
         PurchaseCompleteController purchaseController = loader.getController();
 
+
+
         //Arrange all the panes
         Pane[] children = {storeFront,cart,checkOut,purchaseCompletePane};
         children_ = children;
@@ -245,5 +247,6 @@ public class Main extends Application {
         cartController.clearCart();
         stage.setScene(mainScene);
         gotoStore();
+        cartController.move_button.setText(toCheckout);
     }
 }
